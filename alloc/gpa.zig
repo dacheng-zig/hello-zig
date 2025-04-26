35e7788c547225a8640d5cb06a2a544e01641663
Dacheng Gao <successgdc@gmail.com>
2025-04-26 23:10:46 +0800

    example: GeneralPurposeAllocator

---
+++ alloc/gpa.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const val = try allocator.create(i32);
+    defer allocator.destroy(val);
+
+    val.* = 51;
+    std.debug.print("pointer: {}\n", .{val});
+    std.debug.print("value: {}\n", .{val.*});
+}
